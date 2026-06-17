88b25fe8395b31209473239269c537a31d5fb92e
Victor-valencia-cuer <valenciacuervovictorferney@gmail.com>
2026-06-17 09:13:50 -0500

    pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/anuncio-mango/',
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+  },
 });
